544568ed7d736c9757e91b5169360fc6f5075462
21005984 <vaishuvel19@gmail.com>
2024-07-28 23:06:36 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "https://github.com/21005984/react-day-3.git"
+  base : '/react-day-3/'
 })
